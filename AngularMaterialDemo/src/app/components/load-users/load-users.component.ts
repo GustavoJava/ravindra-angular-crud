5606d04deffecd21ba3db5c2a16a87765ea19af0
GustavoJava <gustavo.java2000@gmail.com>
2023-11-16 09:20:49 -0300

    error messages settings

--- AngularMaterialDemo/src/app/components/load-users/load-users.component.ts
+++ AngularMaterialDemo/src/app/components/load-users/load-users.component.ts
@@ -10,30 +10,30 @@ import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDeleteDialogComponent } from '../confirm-delete-dialog/confirm-delete-dialog.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Component({
   selector: 'app-load-users',
   templateUrl: './load-users.component.html',
-  styleUrls: ['./load-users.component.scss']
+  styleUrls: ['./load-users.component.scss'],
 })
 export class LoadUsersComponent implements OnInit, AfterViewInit {
-
   dataSource = new MatTableDataSource<UserModel>();
   displayColumns = ['id', 'name', 'email', 'action'];
 
   @ViewChild('empTbSort') empTbSort = new MatSort();
 
-  pageSizeOptions=[3,6,9,12];
+  pageSizeOptions = [3, 6, 9, 12];
   pageSize = 5;
-  pageIndex = 0
+  pageIndex = 0;
   pageLength = 0; //total records in db..
 
   dataSourceFilters = new MatTableDataSource(this.dataSource.data);
 
-  constructor(private userService:UserService,
-              public dialog: MatDialog,
-              private router: Router,
-              private snackBar: MatSnackBar) { }
+  constructor(
+    private userService: UserService,
+    public dialog: MatDialog,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.empTbSort;
@@ -45,12 +45,11 @@ export class LoadUsersComponent implements OnInit, AfterViewInit {
   }
 
   loadUsers() {
-    this.userService.getUsers((this.pageIndex+1),(this.pageSize)).subscribe({
-     next:(response)=>{
-      this.dataSource.data = response.users;
-      this.pageLength = response.count;
-     },error:(err)=>console.log(err)
-    })
+    this.userService.getUsers(this.pageIndex + 1, this.pageSize).subscribe((response) => {
+        this.dataSource.data = response.users;
+        this.pageLength = response.count;
+      },() => this.onError()
+    );
   }
 
   applyFilter(event: Event) {
@@ -60,61 +59,68 @@ export class LoadUsersComponent implements OnInit, AfterViewInit {
       this.dataSource.paginator.firstPage();
     }
   }
-
+  //https://stackblitz.com/angular/dnbermjydavk?file=app%2Ftable-overview-example.html,app%2Ftable-overview-example.ts
   loadDataFilter() {
     this.dataSourceFilters.filterPredicate = function (record, filter) {
       debugger;
       var map = new Map(JSON.parse(filter));
       let isMatch = false;
-      for(let [key,value] of map){
-        isMatch = (value=="All") || (record[key as keyof UserModel] == value);
-        if(!isMatch) return false;
+      for (let [key, value] of map) {
+        isMatch = value == 'All' || record[key as keyof UserModel] == value;
+        if (!isMatch) return false;
       }
       return isMatch;
-    }
+    };
   }
 
-  onBtnEdit(id: number){
-    this.router.navigate(['users/edit/'+id]);
+  onBtnEdit(id: number) {
+    this.router.navigate(['users/edit/' + id]);
   }
 
   onBtnDelete(user: UserModel) {
     const dialogRef = this.dialog.open(ConfirmDeleteDialogComponent, {
-      data: `Are you sure delete user ${user.name} ?`
+      data: `Are you sure delete user ${user.name} ?`,
     });
 
     dialogRef.afterClosed().subscribe((result: boolean) => {
-      if (result) {
-        this.userService.delete(user.id).subscribe({
-              next:(response)=> {
-                if(this.dataSource.data.length === 1 && this.pageIndex>0)
-                  this.pageIndex--;
-                  this.onSucess();
-                  this.loadUsers();
-              }
-            })
-          }
-    }, error => this.onError());
+        if (result) {
+          this.userService.delete(user.id).subscribe(()=>{
+              if (this.dataSource.data.length === 1 && this.pageIndex > 0)
+                this.pageIndex--;
+              this.onSucess();
+              this.loadUsers();
+          },()=> this.onError());
+        }
+      }
+    );
   }
 
-  changePage(event:PageEvent){
-    this.pageSize=event.pageSize;
-    this.pageIndex=event.pageIndex;
+  changePage(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
     this.loadUsers();
   }
 
-  onSucess(){
-    this.snackBar.open('Operação realizada com sucesso!','X', {
-          duration: 3000,
-          verticalPosition: 'top'
-    })
+  onSucess() {
+    this.snackBar.open('Operação realizada com sucesso!', 'X', {
+      duration: 3000,
+      verticalPosition: 'top',
+    });
+    this.reload();
   }
 
-  onError(){
-    this.snackBar.open('Erro ao realizar operação!','X', {
-          duration: 3000,
-          verticalPosition: 'top'
-    })
+  onError() {
+    this.snackBar.open('Erro ao realizar operação!', 'X', {
+      duration: 3000,
+      verticalPosition: 'top',
+    });
+    this.reload();
+  }
+
+  reload() {
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
   }
 
 }
